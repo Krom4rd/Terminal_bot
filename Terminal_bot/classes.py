268017f88b530dfebda8515400550ba0239cbd71
Krom4rd <krom4rd@gmail.com>
2024-02-15 07:52:04 +0100

    v 0.0.2

--- Terminal_bot/classes.py
+++ Terminal_bot/classes.py
@@ -1,16 +1,56 @@
-class Name():
+from datetime import date
+from collections import UserDict
+import re
+
+class Field:
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self)->str:
+        return self._value
+
+    @value.setter
+    def value(self, value: str)-> None:
+        self._value = value
+
+    def __str__(self):
+        return str(self._value)
+
+class Name(Field):
+    def __init__(self, name: str)-> None:
+        super().__init__(name.lower().capitalize())
+
+class Phone(Field):
+    def __init__(self, number: str):
+        pass
+
+class Email(Field):
+    def __init__(self, mail: str):
+        pass
+
+class Address(Field):
+    def __init__(self, address: str):
+        pass
+
+class Birthday(Field):
+    def __init__(self, day: str):
+        pass
+
+class Contact():
+    def __init__(self, name: Name, phone: Phone = None, email: Email = None, address: Address = None, birthday: Birthday = None):
+        pass
+
+class Address_book(UserDict):
     pass
-class Phone():
-    pass
-class Email():
-    pass
-class Address():
-    pass
-class Birthday():
-    pass
-class Address_book():
-    pass
-class Note():
-    pass
-class Tag():
-    pass
+
+class Note(Field):
+    def __init__(self, note: str):
+        pass
+
+class Tag(Field):
+    def __init__(self, tag: str):
+        pass
+
+
